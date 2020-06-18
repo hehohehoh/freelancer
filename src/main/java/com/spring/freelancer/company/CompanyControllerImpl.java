dbee164d4943883cc6c9c88c458b3dfd70f5eed8
sqa923 <67043027+sqa923@users.noreply.github.com>
2020-06-18 15:16:14 +0900

    company

---
+++ src/main/java/com/spring/freelancer/company/CompanyControllerImpl.java
@@ -0,0 +1,21 @@
+package com.spring.freelancer.company;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CompanyControllerImpl implements CompanyController {
+
+	@RequestMapping(value = "/comp/login.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String login(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		String goPage = (String)request.getAttribute("viewName");
+		
+		return goPage;
+	}
+}
