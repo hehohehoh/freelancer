42407c2a03225a3a31b9e70c13f7d63ae4500fa6
dytlalcm815 <67043391+dytlalcm815@users.noreply.github.com>
2020-06-22 19:31:35 +0900

    list . insert 추가

--- src/main/java/com/spring/freelancer/free/FreeController.java
+++ src/main/java/com/spring/freelancer/free/FreeController.java
@@ -5,4 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface FreeController {
 	public String login(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public String freeForm(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView listFreelancers(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
