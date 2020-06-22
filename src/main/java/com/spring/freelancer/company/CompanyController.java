aeb4c1c33e6acf8311cc03daf48535598ad9aaa8
sqa923 <67043027+sqa923@users.noreply.github.com>
2020-06-22 20:23:53 +0900

    삭제

--- src/main/java/com/spring/freelancer/company/CompanyController.java
+++
@@ -1,8 +0,0 @@
-package com.spring.freelancer.company;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CompanyController {
-	public String login(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
