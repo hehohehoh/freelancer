827568788954b713294c5e2c7808aa61a0099ab3
dytlalcm815 <67043391+dytlalcm815@users.noreply.github.com>
2020-06-22 19:32:33 +0900

    list. insert 추가

--- src/main/java/com/spring/freelancer/free/FreeControllerImpl.java
+++ src/main/java/com/spring/freelancer/free/FreeControllerImpl.java
@@ -1,21 +1,63 @@
 package com.spring.freelancer.free;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FreeControllerImpl implements FreeController {
 
+	@Autowired
+	private FreeService freeService;
+	@Autowired
+	FreeVO freeVO;
+	
+	@Override
+	@RequestMapping(value = "/free/listFreelancers.do", method = RequestMethod.GET)
+	public ModelAndView listFreelancers(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = (String) request.getAttribute("viewName");
+		List<FreeVO> freelancersList = freeService.listFreelancers();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("freelancersList", freelancersList);
+		return mav;
+	}
+	
+	
 	@RequestMapping(value = "/free/login.do", method = {RequestMethod.GET, RequestMethod.POST})
 	public String login(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
 		String goPage = (String)request.getAttribute("viewName");
-		
 		return goPage;
 	}
+	@RequestMapping(value = "/free/form.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String freeForm(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {	
+		String goPage = (String)request.getAttribute("viewName");
+		return goPage;
+	}
+	@RequestMapping(value = "/free/addFree.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView addFree(@ModelAttribute("free") FreeVO free, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception {	
+		request.setCharacterEncoding("utf-8");
+		int result = 0;
+		result = freeService.addFree(free);
+		ModelAndView mav = new ModelAndView("redirect:/free/listFreelancers.do");
+
+		return mav;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
