7b29923087fbb8e42cf4ef9c7c205b3f45ea88ad
sqa923 <67043027+sqa923@users.noreply.github.com>
2020-06-24 18:06:52 +0900

    기업 가입 승인

--- src/main/java/com/spring/freelancer/mappers/CompMapper.java
+++ src/main/java/com/spring/freelancer/mappers/CompMapper.java
@@ -1,5 +1,7 @@
 package com.spring.freelancer.mappers;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.spring.freelancer.company.CompanyVO;
@@ -10,4 +12,8 @@ public interface CompMapper {
 	void insertCompInfo(CompanyVO companyVO) throws Exception;
 	
 	int checkOverId(String comp_id) throws Exception;
+	
+	List<CompanyVO> compApproList() throws Exception;
+	
+	void compAppro(String comp_id) throws Exception;
 }
